53e60399e39d527dc7348eda01ec915e96f8f534
Varun <varun.2021@vitstudent.ac.in>
2025-10-08 14:22:05 +0530

    modified:   AFIFO_UVM/afifo_sequence.sv 	modified:   AFIFO_UVM/afifo_test.sv 	modified:   AFIFO_UVM/project_configs.sv

--- AFIFO_UVM/afifo_sequence.sv
+++ AFIFO_UVM/afifo_sequence.sv
@@ -7,7 +7,7 @@ class afifo_write_sequence extends uvm_sequence #(afifo_write_sequence_item);
   virtual task body();
     int count;
     afifo_write_sequence_item item;
-    repeat (`NUM_WRITE_TRANS) begin 
+    repeat (`NUM_WRITE_TRANS_WR) begin 
       item = afifo_write_sequence_item::type_id::create("write_item");
       start_item(item);
       item.randomize() with {winc == 1;}; 
@@ -39,7 +39,7 @@ class afifo_read_sequence extends uvm_sequence #(afifo_read_sequence_item);
   virtual task body();
     int count;
     afifo_read_sequence_item item;
-    repeat (`NUM_READ_TRANS) begin
+    repeat (`NUM_READ_TRANS_WR) begin
       item = afifo_read_sequence_item::type_id::create("read_item");
       start_item(item);
       item.randomize() with {rinc == 1;};
@@ -54,5 +54,54 @@ class afifo_read_sequence extends uvm_sequence #(afifo_read_sequence_item);
     item.randomize() with {rinc == 1;};
     `uvm_info(get_type_name(), $sformatf("[%0t] Read Transaction %0d: rinc=%0b", $time, count, item.rinc), UVM_LOW);
     finish_item(item);
+  endtask
+endclass
+
+class afifo_write_full_sequence extends uvm_sequence #(afifo_write_sequence_item);
+  `uvm_object_utils(afifo_write_full_sequence)
+  function new(string name="afifo_write_full_sequence");
+    super.new(name);
+  endfunction
+ 
+  virtual task body();
+    int count;
+    afifo_write_sequence_item item;
+    repeat (`FIFO_DEPTH+2) begin
+      item = afifo_write_sequence_item::type_id::create("write_item");
+      start_item(item);
+      item.randomize() with {winc == 1;}; 
+      `uvm_info(get_type_name(), $sformatf("[%0t] Write Transaction %0d: winc=%0b, wdata=%0d", $time, count, item.winc, item.wdata), UVM_LOW);
+      finish_item(item);
+      count++;
+    end
+    start_item(item);
+    item.randomize() with {winc == 0;}; 
+    `uvm_info(get_type_name(), $sformatf("[%0t] Write Transaction %0d: winc=%0b, wdata=%0d", $time, count, item.winc, item.wdata), UVM_LOW);
+    finish_item(item);
+  endtask
+endclass
+
+class afifo_read_full_sequence extends uvm_sequence #(afifo_read_sequence_item);
+  `uvm_object_utils(afifo_read_full_sequence)
+  function new(string name="afifo_read_full_sequence");
+    super.new(name);
+  endfunction
+ 
+  virtual task body();
+    int count;
+    afifo_read_sequence_item item;
+    repeat (`NUM_READ_LOW_WTR) begin
+      item = afifo_read_sequence_item::type_id::create("read_item");
+      start_item(item);
+      item.randomize() with {rinc == 0;};
+      `uvm_info(get_type_name(), $sformatf("[%0t] Read Transaction %0d: rinc=%0b", $time, count, item.rinc), UVM_LOW);
+      finish_item(item);
+    end
+    repeat (`NUM_READ_TRANS_WTR) begin
+      start_item(item);
+      item.randomize() with {rinc == 1;};
+      `uvm_info(get_type_name(), $sformatf("[%0t] Read Transaction %0d: rinc=%0b", $time, count, item.rinc), UVM_LOW);
+      finish_item(item);
+    end
   endtask
 endclass
--- AFIFO_UVM/afifo_test.sv
+++ AFIFO_UVM/afifo_test.sv
@@ -31,3 +31,40 @@ class afifo_simultaneous_wr_test extends uvm_test;
   endtask
 endclass
 
+// Write-Then-Read Test 
+class afifo_sequential_wr_test extends uvm_test;
+  `uvm_component_utils(afifo_sequential_wr_test)
+  afifo_env env;
+  afifo_write_full_sequence write_seq;
+  afifo_read_full_sequence read_seq;
+
+  function new(string name = "afifo_sequential_wr_test",uvm_component parent=null);
+    super.new(name,parent);
+  endfunction : new
+
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    env = afifo_env::type_id::create("env", this);
+  endfunction
+
+  virtual function void end_of_elaboration_phase(uvm_phase phase);
+    super.end_of_elaboration_phase(phase);
+    uvm_top.print_topology();
+  endfunction
+
+  virtual task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+    phase.raise_objection(this);
+
+    write_seq = afifo_write_full_sequence::type_id::create("write_seq");
+    read_seq = afifo_read_full_sequence::type_id::create("read_seq");
+    fork
+      write_seq.start(env.write_agent.sequencer);
+      read_seq.start(env.read_agent.sequencer);
+    join
+    `uvm_info(get_type_name(), "Sequential Write-Then-Read Test Completed", UVM_LOW)
+    phase.drop_objection(this);
+  endtask
+endclass
+
+
